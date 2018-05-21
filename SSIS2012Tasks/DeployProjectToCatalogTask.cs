10065518e08f285afb0ed2da10cc5a30771c9d68
Jan Abramek <abramek.j@gmail.com>
2018-05-21 10:06:24 +0200

    Upgraded to VS2017 and .NET 4.6

--- SSIS2012Tasks/DeployProjectToCatalogTask.cs
+++ SSIS2012Tasks/DeployProjectToCatalogTask.cs
@@ -1,42 +1,42 @@
-﻿using System;
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+using System;
 using System.Data;
 using System.Data.SqlClient;
 using System.IO;
-using Microsoft.Build.Framework;
-using Microsoft.Build.Utilities;
 
 namespace Microsoft.SqlServer.IntegrationServices.Build
 {
-	/// <summary>
-	/// This Task connects to an SSIS Catalog and deploys the given project files.
-	/// Ensure that the account running MSBuild has permission to deploy to the catalog.
-	/// </summary>
-	public class DeployProjectToCatalogTask : Task
-	{
-		/// <summary>
-		/// One or more paths to .ispac deployment files.
-		/// </summary>
-		[Required]
-		public ITaskItem[] DeploymentFile { get; set; }
+    /// <summary>
+    /// This Task connects to an SSIS Catalog and deploys the given project files.
+    /// Ensure that the account running MSBuild has permission to deploy to the catalog.
+    /// </summary>
+    public class DeployProjectToCatalogTask : Task
+    {
+        /// <summary>
+        /// One or more paths to .ispac deployment files.
+        /// </summary>
+        [Required]
+        public ITaskItem[] DeploymentFile { get; set; }
 
-		/// <summary>
-		/// The SQL instance name of the SSIS Catalog to deploy to.
-		/// </summary>
-		[Required]
-		public string Instance { get; set; }
+        /// <summary>
+        /// The SQL instance name of the SSIS Catalog to deploy to.
+        /// </summary>
+        [Required]
+        public string Instance { get; set; }
 
-		/// <summary>
-		/// The folder on the catalog to deploy to.
-		/// If this folder does not exist, it will be created if <see cref="CreateFolder"/> is true.
-		/// </summary>
-		[Required]
-		public string Folder { get; set; }
+        /// <summary>
+        /// The folder on the catalog to deploy to.
+        /// If this folder does not exist, it will be created if <see cref="CreateFolder"/> is true.
+        /// </summary>
+        [Required]
+        public string Folder { get; set; }
 
-		/// <summary>
-		/// Should the SSIS Catalog Folder be created if it is not already there. 
-		/// This property is optional. The default value is true.
-		/// </summary>
-		public bool CreateFolder { get; set; }
+        /// <summary>
+        /// Should the SSIS Catalog Folder be created if it is not already there.
+        /// This property is optional. The default value is true.
+        /// </summary>
+        public bool CreateFolder { get; set; }
 
 
         /// <summary>
@@ -53,94 +53,97 @@ namespace Microsoft.SqlServer.IntegrationServices.Build
 
 
         /// <summary>
-		/// The name of the SSIS catalog to deploy to.
-		/// This property is optional. The default value is "SSISDB".
-		/// </summary>
-		public string Catalog { get; set; }
+        /// The name of the SSIS catalog to deploy to.
+        /// This property is optional. The default value is "SSISDB".
+        /// </summary>
+        public string Catalog { get; set; }
 
-		public DeployProjectToCatalogTask()
-		{
-			Catalog = "SSISDB";
-			CreateFolder = true;
+        public DeployProjectToCatalogTask()
+        {
+            Catalog = "SSISDB";
+            CreateFolder = true;
             SqlCommandTimeout = 300;
-		}
+        }
 
-		public override bool Execute()
-		{
-			bool result = true;
-			var csb = new SqlConnectionStringBuilder
-			          	{
-			          		DataSource = Instance, IntegratedSecurity = true, InitialCatalog = Catalog, ConnectTimeout = 600
-			          	};
+        public override bool Execute()
+        {
+            bool result = true;
+            var csb = new SqlConnectionStringBuilder
+            {
+                DataSource = Instance,
+                IntegratedSecurity = true,
+                InitialCatalog = Catalog,
+                ConnectTimeout = 600
+            };
 
-			Log.LogMessage(SR.ConnectingToServer, csb.ConnectionString);
+            Log.LogMessage(SR.ConnectingToServer, csb.ConnectionString);
 
-			using (var conn = new SqlConnection(csb.ConnectionString))
-			{
-				try
-				{
-					conn.Open();
-				}
-				catch (Exception e)
-				{
-					Log.LogError(SR.ConnectionError);
-					Log.LogErrorFromException(e);
-					return false;
-				}
+            using (var conn = new SqlConnection(csb.ConnectionString))
+            {
+                try
+                {
+                    conn.Open();
+                }
+                catch (Exception e)
+                {
+                    Log.LogError(SR.ConnectionError);
+                    Log.LogErrorFromException(e);
+                    return false;
+                }
 
-				foreach (var taskItem in DeploymentFile)
-				{
-					try
-					{
-						Log.LogMessage("------");
+                foreach (var taskItem in DeploymentFile)
+                {
+                    try
+                    {
+                        Log.LogMessage("------");
 
-						string projectPath = taskItem.ItemSpec;
+                        string projectPath = taskItem.ItemSpec;
 
-						if (CreateFolder)
-						{
-							EnsureFolderExists(conn, Folder);
-						}
+                        if (CreateFolder)
+                        {
+                            EnsureFolderExists(conn, Folder);
+                        }
 
                         this.EnsureEnvironmentExists(conn, Folder, Environment);
 
-						string projectName = Path.GetFileNameWithoutExtension(projectPath);
-						var bytes = File.ReadAllBytes(projectPath);
+                        string projectName = Path.GetFileNameWithoutExtension(projectPath);
+                        var bytes = File.ReadAllBytes(projectPath);
 
                         var deploymentCmd = GetDeploymentCommand(conn, Folder, projectName, bytes, SqlCommandTimeout);
 
-						try
-						{
-							Log.LogMessage(SR.DeployingProject, projectPath);
-							deploymentCmd.ExecuteNonQuery();
-						}
-						catch (Exception)
-						{
-							Log.LogError(SR.DeploymentFailed);
-							throw;
-						}
+                        try
+                        {
+                            Log.LogMessage(SR.DeployingProject, projectPath);
+                            deploymentCmd.ExecuteNonQuery();
+                        }
+                        catch (Exception)
+                        {
+                            Log.LogError(SR.DeploymentFailed);
+                            throw;
+                        }
 
                         this.EnsureEnvironmentReferenceExists(conn, projectName, Folder, Environment);
 
 
-					}
-					catch (Exception e)
-					{
-						Log.LogErrorFromException(e, true);
-						result = false;
-					}
-				}
-			}
+                    }
+                    catch (Exception e)
+                    {
+                        Log.LogErrorFromException(e, true);
+                        result = false;
+                    }
+                }
+            }
 
-			return result;
-		}
+            return result;
+        }
 
-		private void EnsureFolderExists(SqlConnection connection, string folder)
-		{
+        private void EnsureFolderExists(SqlConnection connection, string folder)
+        {
             if (!FolderExists(connection, folder, SqlCommandTimeout))
-			{
-				CreateCatalogFolder(connection, folder);
-			}
-		}
+            {
+                CreateCatalogFolder(connection, folder);
+            }
+        }
 
         private void EnsureEnvironmentExists(SqlConnection connection, string folder, string environment)
         {
@@ -183,15 +186,15 @@ namespace Microsoft.SqlServer.IntegrationServices.Build
         }
 
         private void CreateCatalogFolder(SqlConnection connection, string folder)
-		{
-			var cmd = new SqlCommand("[catalog].[create_folder]", connection) {CommandType = CommandType.StoredProcedure};
+        {
+            var cmd = new SqlCommand("[catalog].[create_folder]", connection) { CommandType = CommandType.StoredProcedure };
             cmd.CommandTimeout = SqlCommandTimeout;
             cmd.CommandTimeout = SqlCommandTimeout;
-			cmd.Parameters.AddWithValue("folder_name", folder);
+            cmd.Parameters.AddWithValue("folder_name", folder);
 
-			Log.LogMessage(SR.CreatingFolder, folder);
-			cmd.ExecuteNonQuery();
-		}
+            Log.LogMessage(SR.CreatingFolder, folder);
+            cmd.ExecuteNonQuery();
+        }
 
         private void CreateCatalogEnvironment(SqlConnection connection, string folder, string environment)
         {
@@ -220,13 +223,13 @@ namespace Microsoft.SqlServer.IntegrationServices.Build
 
 
         private static SqlCommand GetFolderCommand(SqlConnection connection, string folder, int SqlCommandTimeout)
-		{
-			var cmd = new SqlCommand("SELECT folder_id FROM [catalog].[folders] WHERE name = @FolderName", connection);
+        {
+            var cmd = new SqlCommand("SELECT folder_id FROM [catalog].[folders] WHERE name = @FolderName", connection);
             cmd.CommandTimeout = SqlCommandTimeout;
-			cmd.Parameters.AddWithValue("@FolderName", folder);
+            cmd.Parameters.AddWithValue("@FolderName", folder);
 
-			return cmd;
-		}
+            return cmd;
+        }
 
         private static SqlCommand GetEnvironmentCommand(SqlConnection connection, string folder, string environment, int SqlCommandTimeout)
         {
@@ -250,16 +253,16 @@ namespace Microsoft.SqlServer.IntegrationServices.Build
         }
 
         private static SqlCommand GetDeploymentCommand(SqlConnection connection, string folder, string name, byte[] project, int SqlCommandTimeout)
-		{
-			// build the deployment command
-			var cmd = new SqlCommand("[catalog].[deploy_project]", connection) { CommandType = CommandType.StoredProcedure };
+        {
+            // build the deployment command
+            var cmd = new SqlCommand("[catalog].[deploy_project]", connection) { CommandType = CommandType.StoredProcedure };
             cmd.CommandTimeout = SqlCommandTimeout;
-			cmd.Parameters.AddWithValue("folder_name", folder);
-			cmd.Parameters.AddWithValue("project_name", name);
-			cmd.Parameters.AddWithValue("project_stream", project);
-			cmd.Parameters.AddWithValue("operation_id", SqlDbType.BigInt).Direction = ParameterDirection.Output;
+            cmd.Parameters.AddWithValue("folder_name", folder);
+            cmd.Parameters.AddWithValue("project_name", name);
+            cmd.Parameters.AddWithValue("project_stream", project);
+            cmd.Parameters.AddWithValue("operation_id", SqlDbType.BigInt).Direction = ParameterDirection.Output;
 
-			return cmd;
-		}
-	}
+            return cmd;
+        }
+    }
 }
--- SSIS2012Tasks/Serialization.cs
+++ SSIS2012Tasks/Serialization.cs
@@ -1,158 +1,158 @@
-﻿using System;
+﻿using Microsoft.SqlServer.Dts.Runtime;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Xml;
 using System.Xml.Schema;
 using System.Xml.Serialization;
-using Microsoft.SqlServer.Dts.Runtime;
 
 namespace Microsoft.SqlServer.IntegrationServices.Build
 {
-	public class Xml
-	{
-		public const string NS = "www.microsoft.com/SqlServer/SSIS";
-		public const string Dts = "www.microsoft.com/SqlServer/Dts";
-	}
+    public class Xml
+    {
+        public const string NS = "www.microsoft.com/SqlServer/SSIS";
+        public const string Dts = "www.microsoft.com/SqlServer/Dts";
+    }
 
-	[XmlRootAttribute("Project", Namespace = Xml.NS)]
-	public class ProjectManifest
-	{
-		[XmlAttribute(Namespace = Xml.NS, Form = XmlSchemaForm.Qualified)]
-		public DTSProtectionLevel ProtectionLevel { get; set; }
-		[XmlElement(Namespace = Xml.NS)]
-		public Properties Properties { get; set; }
-		public PackageManifest[] Packages { get; set; }
-		public ConnectionManager[] ConnectionManagers { get; set; }
-	}
+    [XmlRootAttribute("Project", Namespace = Xml.NS)]
+    public class ProjectManifest
+    {
+        [XmlAttribute(Namespace = Xml.NS, Form = XmlSchemaForm.Qualified)]
+        public DTSProtectionLevel ProtectionLevel { get; set; }
+        [XmlElement(Namespace = Xml.NS)]
+        public Properties Properties { get; set; }
+        public PackageManifest[] Packages { get; set; }
+        public ConnectionManager[] ConnectionManagers { get; set; }
+    }
 
-	// [XmlType("Properties", Namespace = Xml.NS)]
-	public class Properties : Dictionary<string, string>, IXmlSerializable
-	{
-		public XmlSchema GetSchema()
-		{
-			return null;
-		}
+    // [XmlType("Properties", Namespace = Xml.NS)]
+    public class Properties : Dictionary<string, string>, IXmlSerializable
+    {
+        public XmlSchema GetSchema()
+        {
+            return null;
+        }
 
-		public void ReadXml(XmlReader reader)
-		{
-			bool empty = reader.IsEmptyElement;
-			reader.Read();
-			if (empty)
-			{
-				return;
-			}
+        public void ReadXml(XmlReader reader)
+        {
+            bool empty = reader.IsEmptyElement;
+            reader.Read();
+            if (empty)
+            {
+                return;
+            }
 
-			while (reader.NodeType != XmlNodeType.EndElement)
-			{
-				if (reader.LocalName == "Property")
-				{
-					string name = reader.GetAttribute("Name", Xml.NS);
-					string value = reader.ReadString();
+            while (reader.NodeType != XmlNodeType.EndElement)
+            {
+                if (reader.LocalName == "Property")
+                {
+                    string name = reader.GetAttribute("Name", Xml.NS);
+                    string value = reader.ReadString();
 
-					Add(name, value);
+                    Add(name, value);
 
-					reader.ReadEndElement();
-				}
-			}
+                    reader.ReadEndElement();
+                }
+            }
 
-			reader.ReadEndElement();
-		}
+            reader.ReadEndElement();
+        }
 
-		public void WriteXml(XmlWriter writer)
-		{
-			throw new NotImplementedException();
-		}
-	}
+        public void WriteXml(XmlWriter writer)
+        {
+            throw new NotImplementedException();
+        }
+    }
 
-	[XmlType("Package", Namespace = Xml.NS)]
-	public class PackageManifest
-	{
-		[XmlAttribute(Namespace = Xml.NS, Form = XmlSchemaForm.Qualified)]
-		public string Name { get; set; }
-		[XmlAttribute(Namespace = Xml.NS, Form = XmlSchemaForm.Qualified)]
-		public bool EntryPoint { get; set; }
-	}
+    [XmlType("Package", Namespace = Xml.NS)]
+    public class PackageManifest
+    {
+        [XmlAttribute(Namespace = Xml.NS, Form = XmlSchemaForm.Qualified)]
+        public string Name { get; set; }
+        [XmlAttribute(Namespace = Xml.NS, Form = XmlSchemaForm.Qualified)]
+        public bool EntryPoint { get; set; }
+    }
 
-	[XmlType("ConnectionManager", Namespace = Xml.NS)]
-	public class ConnectionManager
-	{
-		[XmlAttribute(Namespace = Xml.NS, Form = XmlSchemaForm.Qualified)]
-		public string Name { get; set; }
-	}
+    [XmlType("ConnectionManager", Namespace = Xml.NS)]
+    public class ConnectionManager
+    {
+        [XmlAttribute(Namespace = Xml.NS, Form = XmlSchemaForm.Qualified)]
+        public string Name { get; set; }
+    }
 
-	[XmlRoot("Parameters", Namespace = Xml.NS)]
-	public class ProjectParameters : IXmlSerializable
-	{
-		public List<Parameter> Parameters { get; set; } 
+    [XmlRoot("Parameters", Namespace = Xml.NS)]
+    public class ProjectParameters : IXmlSerializable
+    {
+        public List<Parameter> Parameters { get; set; }
 
-		public XmlSchema GetSchema()
-		{
-			return null;
-		}
+        public XmlSchema GetSchema()
+        {
+            return null;
+        }
 
-		public ProjectParameters()
-		{
-			Parameters = new List<Parameter>();
-		}
+        public ProjectParameters()
+        {
+            Parameters = new List<Parameter>();
+        }
 
-		public void ReadXml(XmlReader reader)
-		{
-			bool empty = reader.IsEmptyElement;
-			reader.Read();
-			if (empty)
-			{
-				return;
-			}
+        public void ReadXml(XmlReader reader)
+        {
+            bool empty = reader.IsEmptyElement;
+            reader.Read();
+            if (empty)
+            {
+                return;
+            }
 
-			var serializer = new XmlSerializer(typeof (Parameter));
+            var serializer = new XmlSerializer(typeof(Parameter));
 
-			while (reader.NodeType != XmlNodeType.EndElement)
-			{
-				string outerXml = reader.ReadOuterXml();
-				
-				var stringReader = new StringReader(outerXml);
-				var parameter = (Parameter) serializer.Deserialize(stringReader);
-				parameter.OuterXml = outerXml;
+            while (reader.NodeType != XmlNodeType.EndElement)
+            {
+                string outerXml = reader.ReadOuterXml();
 
-				Parameters.Add(parameter);
-			}
+                var stringReader = new StringReader(outerXml);
+                var parameter = (Parameter)serializer.Deserialize(stringReader);
+                parameter.OuterXml = outerXml;
 
-			reader.ReadEndElement();
-		}
+                Parameters.Add(parameter);
+            }
 
-		public void WriteXml(XmlWriter writer)
-		{
-			throw new NotImplementedException();
-		}
-	}
+            reader.ReadEndElement();
+        }
 
-	[XmlRoot("Parameter", Namespace = Xml.NS)]
-	public class Parameter
-	{
-		[XmlAttribute(Namespace = Xml.NS, Form = XmlSchemaForm.Qualified)]
-		public string Name { get; set; } 
+        public void WriteXml(XmlWriter writer)
+        {
+            throw new NotImplementedException();
+        }
+    }
 
-		[XmlElement(Namespace = Xml.NS)]
-		public Properties Properties { get; set; }
+    [XmlRoot("Parameter", Namespace = Xml.NS)]
+    public class Parameter
+    {
+        [XmlAttribute(Namespace = Xml.NS, Form = XmlSchemaForm.Qualified)]
+        public string Name { get; set; }
 
-		internal string OuterXml { get; set; } 
+        [XmlElement(Namespace = Xml.NS)]
+        public Properties Properties { get; set; }
 
-		public XmlNode GetXml()
-		{
-			var document = new XmlDocument();
-			document.LoadXml(OuterXml);
+        internal string OuterXml { get; set; }
 
-			return document.FirstChild;
-		}
-	}
+        public XmlNode GetXml()
+        {
+            var document = new XmlDocument();
+            document.LoadXml(OuterXml);
 
-	[XmlRoot("ConnectionManager", Namespace = Xml.Dts)]
-	public class ProjectConnectionManager
-	{
-		[XmlAttribute(Namespace = Xml.Dts, Form = XmlSchemaForm.Qualified)]
-		public string ObjectName { get; set; } 
+            return document.FirstChild;
+        }
+    }
 
-		[XmlAttribute(Namespace = Xml.Dts, Form = XmlSchemaForm.Qualified)]
-		public string CreationName { get; set; } 
-	}
+    [XmlRoot("ConnectionManager", Namespace = Xml.Dts)]
+    public class ProjectConnectionManager
+    {
+        [XmlAttribute(Namespace = Xml.Dts, Form = XmlSchemaForm.Qualified)]
+        public string ObjectName { get; set; }
+
+        [XmlAttribute(Namespace = Xml.Dts, Form = XmlSchemaForm.Qualified)]
+        public string CreationName { get; set; }
+    }
 }
